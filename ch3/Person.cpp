fad237e96169b1d5e462743825d3411db3fbd863
Shujie CHEN <402696415@qq.com>
2016-07-29 09:59:42 -0500

    Person

--- ch3/Person.cpp
+++ ch3/Person.cpp
@@ -1,1 +1,25 @@
 // page 87
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+private:
+	string lastName;
+	string firstName;
+	int age;
+public:
+	Person(string last, string first, int a):
+		lastName(last),firstName(first),age(a){}
+
+	void displayPerson()
+	{
+		cout << "Last name:" << lastName;
+		cout << ", First name:" << firstName;
+		cout << ", Age:" << age << endl;
+	}
+
+	string getLast(){return lastName;}
+};
