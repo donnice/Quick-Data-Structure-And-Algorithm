0d87bb3b1401dd9f94492cde7597abd6a3c5d3a8
Shujie CHEN <402696415@qq.com>
2016-08-01 11:51:15 -0500

    objectSort

--- ch5/objectSort.cpp
+++ ch5/objectSort.cpp
@@ -28,5 +28,44 @@ class ArrayInOb
 {
 private:
 	vector<Person*> v;			// vector of persons
+	int nElems;
+public:
+	ArrayInOb(int max):nElems(0)
+	{
+		v.resize(max);			// resize the vector
+	}
 
+	void insert(string last, string first, int age)
+	{
+		v[nElems] = new Person(last,first,age);
+		nElems++;
+	}
+
+	void display()
+	{
+		for(int j=0;j<nElems;j++)
+			v[j]->displayPerson();
+	}
+
+	void insertionSort()
+	{
+		int in,out;
+
+		for(out=1;out<nElems;out++)
+		{	
+			Person* temp = v[out];
+			in = out;
+
+			while(in>0 && v[in-1]->getLast() > temp-> getLast())
+			{
+				v[in] = v[in-1];
+				--in;
+			}
+		}
+	}
 };
+
+int main()
+{
+	return 0;
+}
