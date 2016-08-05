44d2c291e34b8fe14be18851be6efb5a1b13404a
Shujie CHEN <402696415@qq.com>
2016-08-05 09:52:24 -0500

    link

---
+++ ch8/Link.cpp
@@ -0,0 +1,15 @@
+class Link
+{
+public:
+	int iData;				// data item
+	double dData;			// data item
+	Link* pNext;			// ptr to next link in list
+
+	Link(int id, double dd):// constructor
+		iData(id),dData(dd),pNext(NULL){}
+	
+	void displayLink()
+	{
+		cout << "{" << iData << "," << dData << "}";
+	}
+};
