3838a07da766159b913c3c81d3afacbc19d588fa
Shujie CHEN <402696415@qq.com>
2016-08-16 10:12:52 -0500

    doubly

--- ch10/doublyLinked.cpp
+++ ch10/doublyLinked.cpp
@@ -53,4 +53,89 @@ public:
 		pNewLink->pNext = pFirst;
 		pFirst = pNewLink;
 	}
+
+	void insertLast(double dd)
+	{
+		Link* pNewLink = new Link(dd);
+		if(isEmpty())
+			pFirst = pNewLink;
+		else
+		{
+			pLast->pNext = pNewLink;
+			pNewLink->pPrevious = pLast;
+		}
+		pLast = pNewLink;
+	}
+
+	void removeFirst()
+	{
+		Link* pTemp = pFirst;
+		if(pFirst->pNext == NULL)
+			pLast = NULL;
+		else
+			pFirst->pNext->pPrevious = NULL;
+		pFirst = pFirst->pNext;
+		delete pTemp;
+	}
+
+	void removeLast()
+	{
+		Link* pTemp = pLast;
+		if(pFirst->pNext == NULL)
+			pFirst = NULL;
+		else
+			pLast->pPrevious->pNext = NULL;
+		pLast = pLast->pPrevious;
+		delete pTemp;
+	}
+
+	bool insertAfter(double key, double dd)
+	{
+		Link* pCurrent = pFirst;
+		while(pCurrent->dData != key)
+		{
+			pCurrent = pCurrent->pNext;
+			if(pCurrent == NULL)
+				return false;
+		}
+		Link* pNewLink = new Link(dd);
+		if(pCurrent == pLast)
+		{
+			pNewLink->pNext = NULL;
+			pLast = pNewLink;
+		}
+		else
+		{
+			pNewLink->pNext = pCurrent->pNext;
+			pCurrent->pNext->pPrevious = pNewLink;
+		}
+		pNewLink->pPrevious = pCurrent;
+		pCurrent->pNext = pNewLink;
+		return true;
+	}
+	bool removeKey(double key)
+	{
+		Link* pCurrent = pFirst;
+		while(pCurrent->dData!=key)
+		{
+			pCurrent = pCurrent->pNext;
+			if(pCurrent == NULL)
+				return false;
+		}
+		if(pCurrent == pFirst)
+			pFirst = pCurrent->pNext;
+		else
+			pCurrent->pPrevious->pNext = pCurrent->pNext;
+		if(pCurrent == pLast)
+			pLast = pCurrent->pPrevious;
+		else
+			pCurrent->pNext->pPrevious = pCurrent->pPrevious;
+		delete pCurrent;
+		return true;
+	}
 };
+
+int main()
+{
+	return 0;
+}
