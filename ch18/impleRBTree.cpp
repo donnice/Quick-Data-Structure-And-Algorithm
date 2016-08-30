6f2575d2f29d81bcdd1de0bba04b1a098a189d6e
Shujie CHEN <402696415@qq.com>
2016-08-30 11:59:12 -0500

    delete cases

--- ch18/impleRBTree.cpp
+++ ch18/impleRBTree.cpp
@@ -100,3 +100,126 @@ void insert_case5(struct node *n)
 	else
 		rotate_left(g);
 }
+
+struct node *sibling(struct node *n)
+{
+	if((n == NULL) || (n->parent == NULL))
+		return NULL;
+
+	if(n == n->parent-left)
+		return n->parent->right;
+	else
+		return n->parent->pleft;
+}
+
+void delete_one_child(struct node *n)
+{
+	struct node *child = is_leaf(n->right)? n->left:n->right;
+
+	replace_node(n,child);
+	if(n->color == BLACK)
+	{
+		if(child->color == RED)
+			child->color = BLACK;
+		else
+			delete_case1(child);
+	}
+	free(n);
+}
+
+// case 1: N is a new root
+// we removed one black node and the new root is black
+
+void delete_case1(struct node *n)
+{
+	if(n->parent != NULL)
+		delete_case2(n);
+}
+
+// N is the left child of P
+// S(silbing) is red
+// reverse the colors of P and S and left rotate left at P
+// turning S into N's grandparent
+// P has to be black as it had a red child
+
+void delete_case2(struct node *n)
+{
+	struct node *s = sibling(n);
+
+	if(s->color == RED)
+	{
+		n->parent->color = RED;
+		s->color = BLACK;
+		if(n == n->parent->left)
+			rotate_left(n->parent);
+		else
+			rotate_right(n->parent);
+	}
+	delete_case3(n);
+}
+
+// P,S and S's Children are black
+// repaint S into red
+
+void delete_case3(struct node *n)
+{
+	struct node *s = sibling(n);
+
+	if((n->parent->color == BLACK) &&
+	   (s->color == BLACK) &&
+	   (s->left->color == BLACK) &&
+	   (s->right->color == BLACK))
+	{
+		s->color = RED;
+		delete_case1(n->parent);
+	} else
+		delete_case4(n);
+}
+
+// S and S's Children are balck, but P is red
+// change S and P's color
+void delete_case4(struct node *n)
+{
+	struct node *s = sibling(n);
+
+	if((n->parent->color == RED)&&
+	   (s->color == BLACK) &&
+	   (s->left->color == BLACK) &&
+	   (s->right->color == BLACK)){
+		s->color = RED;
+		n->parent->color=BLACK;
+	} else
+		delete_case5(n);
+}
+
+// S is black, and S's left child is red while right is black
+// N is left child of parent
+// rotate right at S
+// but now N has a black Sibling whose right child is red
+// to case 6
+
+void delete_case5(struct node *n)
+{
+	struct node *s = sibling(n);
+
+	if(s->color == BLACK) {
+		if((n == n->parent->left) &&
+		   (s->right->color == BLACK) &&
+		   (s->left->color == RED)){
+			s->color = RED;
+			s->left->color = BLACK;
+			rotate_right(s);
+		} else if((n == n->parent->right) &&
+				  (s->left->color == BLACK) &&
+				  (s->right->color == RED)){
+		      s->color = RED;
+			  s->right->color = BLACK;
+			  rotate_left(s);
+		}
+	}
+	delete_case6(n);
+}
+
+
+	}
+}
